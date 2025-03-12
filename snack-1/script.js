b3d2cb9e41baafba71318a70505bb0dd36f24372
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-03-12 13:25:56 +0100

    feat: complete snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,12 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+// V 1
+names.forEach(name => { console.log(name); }
+);
+
+// V 2
+names.forEach(name => {
+    console.log(name);
+});
