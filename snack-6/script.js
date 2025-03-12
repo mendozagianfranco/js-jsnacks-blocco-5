de357e1d1d02eb61f4c6284241786a865985f8ba
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-03-12 13:36:30 +0100

    feat: complete snack 6

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,18 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const longCourgettes = zucchine.filter(zucchina => {
+  if (zucchina.length >= 15) {
+    return zucchina;
+  }
+});
+
+const shortCourgettes = zucchine.filter(zucchina => {
+  if (zucchina.length < 15) {
+    return zucchina;
+  }
+});
+
+console.log(longCourgettes);
+console.log(shortCourgettes);
