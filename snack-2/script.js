9979c662f8d2824252e578f96e6933afeac22cb2
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-03-12 13:27:57 +0100

    feat: complete snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -6,3 +6,11 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+// V 1
+people.forEach(person => { console.log(person.name); });
+
+// V 2
+people.forEach(person => {
+  console.log(person.name);
+});
